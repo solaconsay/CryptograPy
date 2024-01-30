e3df85f1ca5b9a6c88e57869c53bde1654b99b61
FAPTAIN L <sherwinlaconsay@gmail.com>
2024-01-29 20:36:10 -0400

    first commit - dns assignment

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ from Crypto.Hash import SHA256
 import os
 
 
-def get_inputs():
-    file_path = input("Please provide the file path: ")
+def get_hashes():
+    file_path = input("Please provide the hash filename: ")
     file_name = os.path.basename(file_path)
     return file_name
-
-def get_file_paths(file_name):
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    relative_file_path = os.path.join(script_directory, file_name)
-    return relative_file_path
 
 
 def compute_hash(file_path):
@@ -21,19 +16,19 @@ def compute_hash(file_path):
     return hash_obj.hexdigest()
 
 def main():
-    file_name = get_inputs()
+    file_name = get_hashes()
     print(file_name)
     relative_file_path = get_file_paths(file_name)
     
 
-    # # Replace 'known_hash' with the actual known hash value
-    # known_hash = '224fc5c451ce0ce8116d38d7a76f25bd3fc40c018967b3f7ed5829b9c9279f33'
+    # Replace 'known_hash' with the actual known hash value
+    known_hash = '224fc5c451ce0ce8116d38d7a76f25bd3fc40c018967b3f7ed5829b9c9279f33'
 
-    # computed_hash = compute_hash(relative_file_path)
-    # if computed_hash == known_hash:
-    #     print(f"The hash of {file_name} matches the known hash.")
-    # else:
-    #     print(f"The hash of {file_name} does not match the known hash.")
+    computed_hash = compute_hash(relative_file_path)
+    if computed_hash == known_hash:
+        print(f"The hash of {file_name} matches the known hash.")
+    else:
+        print(f"The hash of {file_name} does not match the known hash.")
 
 
 
